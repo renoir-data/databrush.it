bc97b7af15a32594f2b672e613b9d9815edefac1
Davide Canali <davide1.canali@mail.polimi.it>
2025-02-10 16:47:35 +0100

    [features] policies

--- src/navigation.ts
+++ src/navigation.ts
@@ -61,10 +61,6 @@ export const footerData = {
       ],
     },
   ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
   socialLinks: [
     { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/data-brush' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/deib-polimi/renoir' },
